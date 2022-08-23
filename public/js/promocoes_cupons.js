9e9677d33979484d318e6594d7591be04b615e8e
Ronan Rodrigues <rounantj@hotmail.com>
2022-08-23 14:51:35 -0300

    fix cms

--- public/js/promocoes_cupons.js
+++ public/js/promocoes_cupons.js
@@ -431,11 +431,14 @@ function getTags(MY_TAGS) {
 
 
     for (const k in MY_TAGS) {
-        var content = '<ul class="listInner listInner2 sub-listInner2 animate__animated ">';
-        html3 += '<li    class="list-item sub-list-item animate__animated ">' + arrowDown4 + '<label style="max-width: 70%; float: left;    margin: 5px 15px ;" class=" subSmart subCheck animate__animated animate__">  ';
-    
-        html3 += MY_TAGS[k] + ' <input class="marcar"  onchange="subTagInputTAG($(this),\'listaTagsFilter\',\'' + MY_TAGS[k]+ '\')" type="checkbox"><span class="checkmark subCheck"></span></label>';
-        html3 += '</li> ';
+        if(MY_TAGS[k] !== ""){
+            var content = '<ul class="listInner listInner2 sub-listInner2 animate__animated ">';
+            html3 += '<li    class="list-item sub-list-item animate__animated ">' + arrowDown4 + '<label style="max-width: 70%; float: left;    margin: 5px 15px ;" class=" subSmart subCheck animate__animated animate__">  ';
+        
+            html3 += MY_TAGS[k] + ' <input class="marcar"  onchange="subTagInputTAG($(this),\'listaTagsFilter\',\'' + MY_TAGS[k]+ '\')" type="checkbox"><span class="checkmark subCheck"></span></label>';
+            html3 += '</li> ';
+        }
+        
     }
 
     return html3
