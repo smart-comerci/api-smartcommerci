81dfe76f69c553a8bc8eabff800c269eec4a0419
Ronan Rodrigues <rounantj@hotmail.com>
2022-10-03 17:09:08 -0300

    fix

--- public/layouts/home.js
+++ public/layouts/home.js
@@ -396,12 +396,13 @@ div3.innerHTML = `
     `;
 
 function getProductCard(data) {
-  console.log(data);
-  let tag = "";
-  if (data.product_site_tags) {
-    tag = data.product_site_tags?.split(",")[0];
-  }
-  const card = `<div class="card-color-preview">
+  if (data) {
+    console.log("os dados", data);
+    let tag = "";
+    if (data.product_site_tags) {
+      tag = data.product_site_tags?.split(",")[0];
+    }
+    const card = `<div class="card-color-preview">
                       <div   >
                         <span     class="card-color-preview_tag" style=" z-index: 2 ;background: var(--color-primary);">
                           ${tag}
@@ -440,7 +441,49 @@ function getProductCard(data) {
                         Adicionar
                       </div>
                 </div> `;
-  return card;
+    return card;
+  } else {
+    const card = `<div class="card-color-preview">
+                      <div   >
+                        <span     class="card-color-preview_tag" style=" z-index: 2 ;background: var(--color-primary);">
+                          FALTA EDITAR
+                        </span>
+                      </div>
+
+                      <div style="  background: url(${notFound}); background-size: cover;     width: 80px;    height: 80px;    margin: -5px auto; " class="card-color-preview_icon">
+                      
+                      </div>
+
+                      <div class="card-color-preview_mark">
+                       FALTA EDITAR
+                      </div>
+
+                      <div class="card-color-preview_title">
+                        FALTA EDITAR
+                      </div>
+
+                      <div class="card-color-preview_units ">
+                        1 unidade
+                      </div>
+
+                      <div class="card-color-preview_stars">
+                        ★★★★★
+                      </div>
+
+                      <div class="card-color-preview_price">
+                        R$ 0,00
+                      </div>
+
+                      <div class="card-color-preview_button">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                          <path d="M112 112C112 50.14 162.1 0 224 0C285.9 0 336 50.14 336 112V160H400C426.5 160 448 181.5 448 208V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V208C0 181.5 21.49 160 48 160H112V112zM160 160H288V112C288 76.65 259.3 48 224 48C188.7 48 160 76.65 160 112V160zM136 256C149.3 256 160 245.3 160 232C160 218.7 149.3 208 136 208C122.7 208 112 218.7 112 232C112 245.3 122.7 256 136 256zM312 208C298.7 208 288 218.7 288 232C288 245.3 298.7 256 312 256C325.3 256 336 245.3 336 232C336 218.7 325.3 208 312 208z"></path>
+                        </svg>
+
+                        Adicionar
+                      </div>
+                </div> `;
+    return card;
+  }
 }
 
 function getRevenueCard() {
