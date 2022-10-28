7a10f43e1ef6662fdf7fb773cc7efaecd080ed0a
Ronan Rodrigues <rounantj@hotmail.com>
2022-10-28 20:47:07 -0300

    asd

--- public/js/categories_new.js
+++ public/js/categories_new.js
@@ -1,5 +1,5 @@
 const api_host = "https://api-smartcomerci.com.br:7070";
-
+getCategories();
 const categoriesObject = {
   affiliateId: Number(localStorage.AFFILIATE_ID),
   masterId: Number(localStorage.MASTER_ID),
@@ -46,14 +46,32 @@ class Subcategory {
     this.id = id;
   }
 }
-
 function addCategory() {
   let categoria = new Category(categoriesObject.categories.length);
   categoriesObject.categories.push(categoria);
   console.log(categoriesObject);
 }
-
-async function publicarAlteracoes() {
+async function getCategories() {
+  const resultado = await $.ajax({
+    type: "GET",
+    url: api_host + "/categorie_find/" + localStorage.AFFILIATE_ID,
+    headers: {
+      "x-access-token": localStorage.token,
+    },
+    data: "",
+    success: function (data) {},
+    error: function (data) {},
+    complete: function () {},
+  });
+  if (!resultado) {
+    const criaPrimeiroAcesso = await criarPrimeiroAcesso();
+    console.log("Primeiro acesso", criaPrimeiroAcesso);
+  } else {
+    categoriesObject = resultado.data;
+  }
+  console.log(resultado, categoriesObject);
+}
+async function criarPrimeiroAcesso() {
   const resultado = await $.ajax({
     type: "POST",
     url: api_host + "/categorie_create",
